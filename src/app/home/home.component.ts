fdaad191840aad83208aa2110706b317b1e787b3
higueraDev <49871959+higueraDev@users.noreply.github.com>
2023-01-26 23:21:07 -0700

    [UPDATED]: home layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  @ViewChild('main') main!: ElementRef<HTMLElement>;
 
+  public sectionActive: number = 0;
+
+  get viewHeight() {
+    return window.visualViewport?.height || 0;
+  }
+
+  get position() {
+    return this.main?.nativeElement.scrollTop || 0;
+  }
+
+  scroll() {
+    this.main.nativeElement.scrollTo({
+      top: this.viewHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  onScroll() {
+    this.sectionActive =  this.position / this.viewHeight;
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,27 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { MaterialModule } from '../material/material.module';
+import { HeroComponent } from './components/hero/hero.component';
+import { SharedModule } from '../shared/shared.module';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { CtaComponent } from './components/cta/cta.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeroComponent,
+    WelcomeComponent,
+    IntroductionComponent,
+    CtaComponent,
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MaterialModule
+    MaterialModule,
+    SharedModule,
   ]
 })
 export class HomeModule { }
