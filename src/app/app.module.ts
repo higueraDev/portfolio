7f4d749a554fdb5d0aac2692936a3ea38e8f0d95
higueraDev <49871959+higueraDev@users.noreply.github.com>
2023-02-01 16:33:15 -0700

    [UPDATED]: angular - firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SharedModule } from './shared/shared.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, LayoutComponent,PageNotFoundComponent],
@@ -17,8 +23,15 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule,
     HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'portfolio-a9c2c',
+    appId: '1:300556222607:web:c8d9e69d2169538da8b0fa',
+    databaseURL: 'https://portfolio-a9c2c-default-rtdb.firebaseio.com',
+    storageBucket: 'portfolio-a9c2c.appspot.com',
+    locationId: 'us-west2',
+    apiKey: 'AIzaSyBptfSSKvjf6sHed-lcLQWQgVj1e6EQb2I',
+    authDomain: 'portfolio-a9c2c.firebaseapp.com',
+    messagingSenderId: '300556222607',
+  },
   production: true,
   apiURL: 'https://portfolio-a9c2c-default-rtdb.firebaseio.com'
 };
